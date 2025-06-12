fd84d2e118eb9dbd9bac7ef54cf19e08df61a28a
hienvl125 <hienviluong125@gmail.com>
2025-06-12 23:02:17 +0700

    update Jun 2025

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 import "./style.css"
 import LoadCV from "./dataloader";
-import { fullstackDevDescriptionVietnamese, fullstackDevDescription } from "./title_description"
+import { backendDevDescription, fullstackDevDescription } from "./title_description"
 
 document.addEventListener("DOMContentLoaded", () => {
-  LoadCV(fullstackDevDescriptionVietnamese);
+  LoadCV(backendDevDescription);
 
-  document.querySelector("#translate-button").addEventListener("click", () => {
-    const lang = document.querySelector("#translate-button").getAttribute("data-lang");
-    if (lang === "en") {
-      LoadCV(fullstackDevDescription);
-      document.querySelector("#translate-button").setAttribute("data-lang", "vi");
-      document.querySelector("#translate-button").innerHTML = "Tiếng Việt";
-    } else {
-      LoadCV(fullstackDevDescriptionVietnamese);
-      document.querySelector("#translate-button").setAttribute("data-lang", "en");
-      document.querySelector("#translate-button").innerHTML = "English";
-    }
-  })
+  // Tạm thời không cần đổi ngôn ngữ
+  // document.querySelector("#translate-button").addEventListener("click", () => {
+  //   const lang = document.querySelector("#translate-button").getAttribute("data-lang");
+  //   if (lang === "en") {
+  //     LoadCV(backendDevDescription);
+  //     document.querySelector("#translate-button").setAttribute("data-lang", "vi");
+  //     document.querySelector("#translate-button").innerHTML = "Tiếng Việt";
+  //   } else {
+  //     LoadCV(backendDevDescription);
+  //     document.querySelector("#translate-button").setAttribute("data-lang", "en");
+  //     document.querySelector("#translate-button").innerHTML = "English";
+  //   }
+  // })
 })
